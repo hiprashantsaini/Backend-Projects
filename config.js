552f7896633d50c4db47850f27a2837fb7c2050b
Prashant <prashantsaini4449@gmail.com>
2024-05-02 10:31:52 +0530

    Files added

---
+++ config.js
@@ -0,0 +1,10 @@
+const SECRET_KEY="This_is_secret";
+const emailUser='prashant@gmail.com';
+const emailPassword='izrrh';
+// These are not correct credentials
+
+module.exports={
+    SECRET_KEY,
+    emailUser,
+    emailPassword
+}
